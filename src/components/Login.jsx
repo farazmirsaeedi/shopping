ba3f29185e48ce9db11ee4e3f4ee2b0112a01825
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-16 20:42:14 +0330

    create Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,21 @@
-const Login = () => {
 
+const Login = () => {
+    return (
+        <>
+            <div className="w-screen h-screen flex justify-center items-center bg-orange-100">
+                <div className="w-72 sm:w-80 p-5 border border-orange-400 rounded-lg bg-white">
+                    <h1 className="text-xl text-center">Log In</h1>
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="user">Username</label>
+                        <input className="mb-2 border border-orange-400 rounded-md p-1" id="user" type="text" />
+                        <label htmlFor="Password">Password</label>
+                        <input className=" border border-orange-400 rounded-md p-1" id="Password" type="text" />
+                    </div>
+                    <button className="w-full mt-6 bg-orange-400 hover:bg-orange-500 transition duration-100 rounded-md text-white uppercase p-2 ">Log In</button>
+                </div>
+            </div>
+        </>
+    )
 }
+
 export default Login;
