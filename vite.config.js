68257bd4bd40dd3473033313ee6d8292a0904a00
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 06:53:16 +1200

    fix bug of gh-pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
     }
 
     if (command !== 'serve') {
-        config.base = '/shoping/'
+        config.base = '/shopping/'
     }
 
     return config
