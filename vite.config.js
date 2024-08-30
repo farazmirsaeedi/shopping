0fbcb3198effe130f7607ef35454518ad04fbc4b
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-08-31 06:55:44 +1200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
     const config = {
+        base: '/shopping',
         plugins: [react()],
-        base: '/',
     }
 
-    if (command !== 'serve') {
-        config.base = '/shopping/'
-    }
 
     return config
 })
